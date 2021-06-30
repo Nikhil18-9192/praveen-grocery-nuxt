61c0f3feb9424c5734839672e4a8b2388edafae5
nikhil18-9192 <nikhilkorane08@gmail.com>
2021-06-30 10:14:24 +0530

    body css change

--- layouts/default.vue
+++ layouts/default.vue
@@ -47,6 +47,25 @@ html {
   box-sizing: border-box;
 }
 
+body {
+  background: #e5e5e5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#__layout {
+  position: relative;
+  width: 375px;
+  background: #fff;
+  height: 100vh;
+  max-height: 812px;
+  overflow: hidden;
+}
 *,
 *::before,
 *::after {
